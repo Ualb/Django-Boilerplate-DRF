4cff8886d8fe9a3fb39cc409de806caf0521ae34
Ulises López <ulises050794@gmail.com>
2021-02-06 18:43:47 -0600

    english version of verbose_name

--- django_boilerplate/apps/users/apps.py
+++ django_boilerplate/apps/users/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'django.contrib.auth'
-    verbose_name = 'Autenticación y Autorización'
+    verbose_name = 'authentication and authorization'
